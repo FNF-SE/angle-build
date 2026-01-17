9df97d89a040b0c909260809446d4767338577d7
MAJigsaw77 <77043862+MAJigsaw77@users.noreply.github.com>
2026-01-17 19:02:19 +0200

    Disable `D3D11`.

--- source/Build.hx
+++ source/Build.hx
@@ -103,6 +103,7 @@ class Build
 					final renderingBackends:Array<String> = [];
 
 					renderingBackends.push('angle_enable_d3d9=false');        // Disable D3D9 backend
+					renderingBackends.push('angle_enable_d3d11=false');       // Disable D3D11 backend
 					renderingBackends.push('angle_enable_gl=false');          // Disable OpenGL backend
 					renderingBackends.push('angle_enable_metal=false');       // Disable Metal backend
 					renderingBackends.push('angle_enable_null=false');        // Disable Null backend
@@ -110,7 +111,6 @@ class Build
 
 					renderingBackends.push('angle_enable_swiftshader=false'); // Disable SwiftShader
 
-					renderingBackends.push('angle_enable_d3d11=true');                     // Enable D3D11 backend
 					renderingBackends.push('angle_enable_vulkan=true');                    // Enable Vulkan backend
 					renderingBackends.push('angle_enable_vulkan_api_dump_layer=false');    // Disable Vulkan API dump layer
 					renderingBackends.push('angle_enable_vulkan_validation_layers=false'); // Disable Vulkan validation layers
